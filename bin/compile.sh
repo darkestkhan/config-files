37b782aa46e5600f814a15e81535f1d5272d781d
darkestkhan <darkestkhan@gmail.com>
2010-08-26 13:01:30 +0200

    few changs to bin/compile.sh

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-gcc-4.4 -O3 -std=gnu99 -fomit-frame-pointer -pipe -j2 -m128bit-long-double -m64 -mssse3 -mfpmath=sse -march=core2 $1 -o $2
+
+case $1 in
+    *.c) gcc-4.4 -O3 -std=gnu99 -fomit-frame-pointer -pipe -j2 -m128bit-long-double -m64 -mssse3 -mfpmath=sse -march=core2 $1 -o $2 ;;
+    *.h) gcc-4.4 -O3 -std=gnu99 -fomit-frame-pointer -pipe -j2 -m128bit-long-double -m64 -mssse3 -mfpmath=sse -march=core2 $1 -o $2 ;;
+    *.cpp) gcc-4.4 -O3 -std=gnu99 -fomit-frame-pointer -pipe -j2 -m128bit-long-double -m64 -mssse3 -mfpmath=sse -march=core2 $1 -o $2 ;;
+    *.hpp) gcc-4.4 -O3 -std=gnu99 -fomit-frame-pointer -pipe -j2 -m128bit-long-double -m64 -mssse3 -mfpmath=sse -march=core2 $1 -o $2 ;;
+    *.ads) gnatmake -O3 $1 ;;
+    *.adb) gnatmake -O3 $1 ;;
+    *.hs) ghc -O3 $1 -o $2 ;;
+    *.lhs) ghc -O3 $1 -o $2 ;;
+    *) echo "wtf?" ;;
+esac
