af8c3ecb11326440f4517e15cb6c5c47959b7363
darkestkhan <darkestkhan@gmail.com>
2010-08-31 16:22:17 +0200

    added fluxbox configs

--- make-install.sh
+++ make-install.sh
@@ -6,6 +6,17 @@ ln -b bin/.ssh-agent.sh $HOME/bin/.ssh-agent.sh
 # mkdir $HOME/bin
 ln -b bin/compile.sh $HOME/bin/compile.sh
 ln -b bin/keyboard.sh $HOME/bin/keyboard.sh
+# mkdir $HOME/.fluxbox
+ln -b .fluxbox/apps $HOME/.fluxbox/apps
+ln -b .fluxbox/fbrun_history $HOME/.fluxbox/fbrun_history
+ln -b .fluxbox/init $HOME/.fluxbox/init
+ln -b .fluxbox/keys $HOME/.fluxbox/keys
+ln -b .fluxbox/lastwallpaper $HOME/.fluxbox/lastwallpaper
+ln -b .fluxbox/menu $HOME/.fluxbox/menu
+ln -b .fluxbox/overlay $HOME/.fluxbox/overlay
+ln -b .fluxbox/slitlist $HOME/.fluxbox/slitlist
+ln -b .fluxbox/startup $HOME/.fluxbox/startup
+ln -b .fluxbox/windowmenu $HOME/.fluxbox/windowmenu
 # files that need root privileges
 ln -b root/cpug.sh $HOME/bin/cpug.sh
 ln -b root/upt.sh $HOME/bin/upt.sh
