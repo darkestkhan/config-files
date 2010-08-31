3c556627f62e3383e27af922eae98ce066b78a6f
darkestkhan <darkestkhan@gmail.com>
2010-08-31 16:15:50 +0200

    little changes to my prompt, and rather big changes in compile.sh

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,13 +1,30 @@
-#!/usr/bin/env bash
+#!/usr/bin/env dash
+
+OBJ=
+STD=gnu99
+CPU=core2
+MACHINE=m64
+THREADS=j2
+OPT=O3
+SSE=mssse3
+LONGDOUBLE=m128bit-long-double
+#if [ $3 = c ] ; then
+#    OBJ=C
+#fi
 
 case $1 in
-    *.c) gcc-4.4 -O3 -std=gnu99 -fomit-frame-pointer -pipe -j2 -m128bit-long-double -m64 -mssse3 -mfpmath=sse -march=core2 $1 -o $2 ;;
-    *.h) gcc-4.4 -O3 -std=gnu99 -fomit-frame-pointer -pipe -j2 -m128bit-long-double -m64 -mssse3 -mfpmath=sse -march=core2 $1 -o $2 ;;
-    *.cpp) gcc-4.4 -O3 -std=gnu99 -fomit-frame-pointer -pipe -j2 -m128bit-long-double -m64 -mssse3 -mfpmath=sse -march=core2 $1 -o $2 ;;
-    *.hpp) gcc-4.4 -O3 -std=gnu99 -fomit-frame-pointer -pipe -j2 -m128bit-long-double -m64 -mssse3 -mfpmath=sse -march=core2 $1 -o $2 ;;
-    *.ads) gnatmake -O3 $1 ;;
-    *.adb) gnatmake -O3 $1 ;;
-    *.hs) ghc -O3 $1 -o $2 ;;
-    *.lhs) ghc -O3 $1 -o $2 ;;
+    *.c) gcc-4.4 -$OPT -std=$STD -fomit-frame-pointer -pipe -$THREADS -$LONGDOUBLE -$MACHINE -$SSE -mfpmath=sse -march=$CPU $1 -o $2 ;;
+    *.h) gcc-4.4 -$OPT -std=$STD -fomit-frame-pointer -pipe -$THREADS -$LONGDOUBLE -$MACHINE -$SSE -mfpmath=sse -march=$CPU $1 -o $2 ;;
+    *.cpp) gcc-4.4 -$OPT -std=$STD -fomit-frame-pointer -pipe -$THREADS -$LONGDOUBLE -$MACHINE -$SSE -mfpmath=sse -march=$CPU $1 -o $2 ;;
+    *.hpp) gcc-4.4 -$OPT -std=$STD -fomit-frame-pointer -pipe -$THREADS -$LONGDOUBLE -$MACHINE -$SSE -mfpmath=sse -march=$CPU $1 -o $2 ;;
+    *.ads) gnatmake -$OPT $1 ;;
+    *.adb) gnatmake -$OPT $1 ;;
+    *.hs) ghc -$OPT $1 -o $2 ;;
+    *.lhs) ghc -$OPT $1 -o $2 ;;
+    *.f90) gfortran -$OPT $1 -o $2 ;;
+    *.f77) gfortran -$OPT $1 -o $2 ;;
+    *.f95) gfortran -$OPT $1 -o $2 ;;
+#    *.cl) sbcl -- not yet ... ;;
+    *.s) gas -$OPT $1 -o $2 ;;
     *) echo "wtf?" ;;
 esac
