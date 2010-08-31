20444175724aeff5582f7cbf5cca0d6e71260d04
darkestkhan <darkestkhan@gmail.com>
2010-09-01 01:24:21 +0200

    added .Xmodmap file (at last no caps lock :), deleted fbrun_history (addidng it was my mistake)

--- make-install.sh
+++ make-install.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
+ln -b .bash_aliases $HOME/.bash_aliases
 ln -b .bashrc $HOME/.bashrc
 ln -b .profile $HOME/.profile
-ln -b .bash_aliases $HOME/.bash_aliases
-ln -b bin/.ssh-agent.sh $HOME/bin/.ssh-agent.sh
-# mkdir $HOME/bin
-ln -b bin/compile.sh $HOME/bin/compile.sh
-ln -b bin/keyboard.sh $HOME/bin/keyboard.sh
+ln -b .Xmodmap $HOME/.Xmodmap
 # mkdir $HOME/.fluxbox
 ln -b .fluxbox/apps $HOME/.fluxbox/apps
-ln -b .fluxbox/fbrun_history $HOME/.fluxbox/fbrun_history
 ln -b .fluxbox/init $HOME/.fluxbox/init
 ln -b .fluxbox/keys $HOME/.fluxbox/keys
 ln -b .fluxbox/lastwallpaper $HOME/.fluxbox/lastwallpaper
@@ -17,6 +13,10 @@ ln -b .fluxbox/overlay $HOME/.fluxbox/overlay
 ln -b .fluxbox/slitlist $HOME/.fluxbox/slitlist
 ln -b .fluxbox/startup $HOME/.fluxbox/startup
 ln -b .fluxbox/windowmenu $HOME/.fluxbox/windowmenu
+# mkdir $HOME/bin
+ln -b bin/.ssh-agent.sh $HOME/bin/.ssh-agent.sh
+ln -b bin/compile.sh $HOME/bin/compile.sh
+ln -b bin/keyboard.sh $HOME/bin/keyboard.sh
 # files that need root privileges
 ln -b root/cpug.sh $HOME/bin/cpug.sh
 ln -b root/upt.sh $HOME/bin/upt.sh
