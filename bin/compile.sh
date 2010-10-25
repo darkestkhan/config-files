0c0883281dff651db1cf310d72825f3b6582734f
darkestkhan <darkestkhan@gmail.com>
2010-10-25 19:43:36 +0000

    really small modifications in bin/compile.sh

--- bin/compile.sh
+++ bin/compile.sh
@@ -4,17 +4,17 @@ OBJ=
 CSTD=gnu99
 CPU=core2
 MACHINE=m64
-THREADS=j2
+THREADS=
 OPT=O2
 SSE=mssse3
 LONGDOUBLE=m128bit-long-double
 FPM='-fomit-frame-pointer -pipe -mfpmath=sse'
 
 case $1 in
-    *.c) gcc-4.4 -$OPT -std=$CSTD $FPM -$THREADS -$LONGDOUBLE -$MACHINE -$SSE -march=$CPU $1 -o $2 ;;
-    *.h) gcc-4.4 -$OPT -std=$CSTD $FPM -$THREADS -$LONGDOUBLE -$MACHINE -$SSE -march=$CPU $1 -o $2 ;;
-    *.cpp) gcc-4.4 -$OPT -std=$CSTD $FPM -$THREADS -$LONGDOUBLE -$MACHINE -$SSE -march=$CPU $1 -o $2 ;;
-    *.hpp) gcc-4.4 -$OPT -std=$CSTD $FPM -$THREADS -$LONGDOUBLE -$MACHINE -$SSE -march=$CPU $1 -o $2 ;;
+    *.c) gcc-4.4 -$OPT -std=$CSTD $FPM -$LONGDOUBLE -$MACHINE -$SSE -march=$CPU $1 -o $2 ;;
+    *.h) gcc-4.4 -$OPT -std=$CSTD $FPM -$LONGDOUBLE -$MACHINE -$SSE -march=$CPU $1 -o $2 ;;
+    *.cpp) gcc-4.4 -$OPT -std=$CSTD $FPM -$LONGDOUBLE -$MACHINE -$SSE -march=$CPU $1 -o $2 ;;
+    *.hpp) gcc-4.4 -$OPT -std=$CSTD $FPM -$LONGDOUBLE -$MACHINE -$SSE -march=$CPU $1 -o $2 ;;
     *.ads) gnatmake -$OPT $1 ;;
     *.adb) gnatmake -$OPT $1 ;;
     *.hs) ghc -$OPT $1 -o $2 ;;
