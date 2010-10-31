a5983ba81e7303a177adbfebb3bd130315ff0c7b
darkestkhan <darkestkhan@gmail.com>
2010-10-31 12:05:04 +0000

    modified cpug.sh in order to make it much more flexible

--- root/cpug.sh
+++ root/cpug.sh
@@ -3,5 +3,12 @@
 MAX=2167000 # defines maximum possible frequency for given cpu (in this case my)
 MIN=1000000 # defines minimum possible frequency for given cpu (in this case my)
 # modprobe acpi_cpufreq
-echo ${MIN} > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo ${MIN} > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+if [ $1 = max ]; then
+    echo ${MAX} > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+    echo ${MAX} > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+fi
+
+if [ $1 = min ]; then
+    echo ${MIN} > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+    echo ${MIN} > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+fi
