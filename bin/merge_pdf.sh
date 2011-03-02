8e7df2a756c44cf9f410db31dc1fdb42cc511a10
darkestkhan <darkestkhan@gmail.com>
2011-03-02 15:00:01 +0000

    added script merging pdf files
    This script depends on gs;
    Thanks to http://everyjoe.com/technology/merge-multiple-pdfs-into-one-file/

---
+++ bin/merge_pdf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#-- This script is merging given pdf files in to one pdf file in given order
+#-- Depends on ghostscript
+
+SPACE=' '
+ARGS=
+OUTPUT=$1
+while [ $# -gt 1 ]
+do
+  shift 1 #-- first arg is output, all other args are pdfs to merge
+  ARGS=${ARGS}${SPACE}$1
+done 
+gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=${OUTPUT} -dBATCH ${ARGS}
+
