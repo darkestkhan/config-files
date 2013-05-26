497407b5edc49dd0169ec08bd57134f3c40f33e5
darkestkhan <darkestkhan@gmail.com>
2013-05-26 09:37:27 +0000

    Small update.
    Signed-off-by: darkestkhan <darkestkhan@gmail.com>

--- make-install.sh
+++ make-install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env dash
 # TODO: Make sure that destination for symlink doesn't exist.
+# Bash confguration
 ln -s ${PWD}/.bashrc ${HOME}/.bashrc
+ln -s ${PWD}/.bash_logout ${HOME}/.bash_logout
 ln -s ${PWD}/.profile ${HOME}/.profile
+ln -s ${PWD}/.gpr_project_path ${HOME}/.gpr_project_path
+# Miscellanous configuration files
 ln -s ${PWD}/.Xmodmap ${HOME}/.Xmodmap
 # Vim cofiguration
 ln -s ${PWD}.vimrc ${HOME}/.vimrc
